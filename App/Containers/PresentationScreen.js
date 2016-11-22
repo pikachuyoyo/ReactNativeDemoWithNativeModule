fc1b6fbd9995cf3ed341aeda1bd538472d13ec93
Shrouk Hasan Khan <shroukkhan@gmail.com>
2016-11-22 11:16:27 +0700

    tv controls

--- App/Containers/PresentationScreen.js
+++ App/Containers/PresentationScreen.js
@@ -1,11 +1,11 @@
 import React, {PropTypes} from 'react'
-import {ScrollView, Text, Image, View, DeviceEventEmitter, Switch} from 'react-native'
-import {Images, Colors} from '../Themes'
+import {ScrollView, Text, Image, View, DeviceEventEmitter, Switch, TouchableOpacity} from 'react-native'
+import {Images, Colors, Metrics} from '../Themes'
 import RoundedButton from '../Components/RoundedButton'
 import {Actions as NavigationActions} from 'react-native-router-flux'
 import FingiSdkActions from '../Redux/FingiSdkRedux'
 import {connect} from 'react-redux'
-
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 // Styles
 import styles from './Styles/PresentationScreenStyle'
@@ -18,7 +18,9 @@ class PresentationScreen extends React.Component {
       light6: false,
       ac1: false,
       ac1Setpoint: '0',
-      ac1Speed: 'LOW'
+      ac1Speed: 'LOW',
+
+      tvPower: false,
 
     }
   }
@@ -70,7 +72,14 @@ class PresentationScreen extends React.Component {
               value={this.state.light6}/>
           </View>
 
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', padding: 15}}>
+          <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            padding: 15,
+            borderBottomWidth: 2,
+            borderColor: 'white'
+          }}>
             <Text style={{fontSize: 18, color: Colors.snow, fontWeight: 'bold'}}>
               AC Meeting Room ( ac-1 )
             </Text>
@@ -81,6 +90,136 @@ class PresentationScreen extends React.Component {
               }}
               value={this.state.ac1}/>
           </View>
+
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', padding: 15}}>
+            <Text style={{fontSize: 18, color: Colors.snow, fontWeight: 'bold'}}>
+              TV Innvue Power ( tv-2 )
+            </Text>
+            <Switch
+              onValueChange={(value) => {
+                this.props.commandToRoomRequest(value ? "POWER tv-2 ON" : "POWER tv-2 OFF");
+                this.setState({tvPower: value})
+              }}
+              value={this.state.tvPower}
+            />
+          </View>
+
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', padding: 15}}>
+            <Text style={{fontSize: 18, color: Colors.snow, fontWeight: 'bold'}}>
+              Volume ( tv-2 )
+            </Text>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginLeft: 20,height:75}}>
+              <TouchableOpacity onPress={()=> {
+                this.props.commandToRoomRequest("VOLUME tv-2 UP");
+              }}>
+                <Icon name='volume-up'
+                      size={Metrics.icons.medium}
+                      color={Colors.snow}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={()=> {
+                this.props.commandToRoomRequest("VOLUME tv-2 DOWN");
+              }}>
+                <Icon name='volume-down'
+                      size={Metrics.icons.medium}
+                      color={Colors.snow}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={()=> {
+                this.props.commandToRoomRequest("MUTE tv-2 ON");
+              }}>
+                <Icon name='volume-off'
+                      size={Metrics.icons.medium}
+                      color={Colors.snow}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', padding: 15,height:75}}>
+            <Text style={{fontSize: 18, color: Colors.snow, fontWeight: 'bold'}}>
+              CHANNEL ( tv-2 )
+            </Text>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginLeft: 20}}>
+              <TouchableOpacity onPress={()=> {
+                this.props.commandToRoomRequest("CHANNEL tv-2 UP");
+              }}>
+                <Icon name='angle-up'
+                      size={Metrics.icons.large}
+                      color={Colors.snow}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={()=> {
+                this.props.commandToRoomRequest("CHANNEL tv-2 DOWN");
+              }}>
+                <Icon name='angle-down'
+                      size={Metrics.icons.large}
+                      color={Colors.snow}
+                />
+              </TouchableOpacity>
+
+            </View>
+          </View>
+
+
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', padding: 15, height: 180}}>
+            <Text style={{fontSize: 18, color: Colors.snow, fontWeight: 'bold'}}>
+              DIRECTIONAL ( tv-2 )
+            </Text>
+            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center',}}>
+
+              <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center',}}>
+                <TouchableOpacity onPress={()=> {
+                  this.props.commandToRoomRequest("KEYPAD tv-2 UP");
+                }}>
+                  <Icon name='angle-up'
+                        size={Metrics.icons.large}
+                        color={Colors.snow}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between',}}>
+                <TouchableOpacity  style={{padding:10,marginRight:20}} onPress={()=> {
+                  this.props.commandToRoomRequest("KEYPAD tv-2 LEFT");
+                }}>
+                  <Icon name='angle-left'
+                        size={Metrics.icons.large}
+                        color={Colors.snow}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity  style={{padding:10}} onPress={()=> {
+                  this.props.commandToRoomRequest("KEYPAD tv-2 ENTER");
+                }}>
+                  <Icon name='check'
+                        size={Metrics.icons.medium}
+                        color={Colors.snow}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity  style={{padding:10,marginLeft:20}} onPress={()=> {
+                  this.props.commandToRoomRequest("KEYPAD tv-2 RIGHT");
+                }}>
+                  <Icon name='angle-right'
+                        size={Metrics.icons.large}
+                        color={Colors.snow}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center',}}>
+                <TouchableOpacity onPress={()=> {
+                  this.props.commandToRoomRequest("KEYPAD tv-2 DOWN");
+                }}>
+                  <Icon name='angle-down'
+                        size={Metrics.icons.large}
+                        color={Colors.snow}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+
+
+
 
           {/*<View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', padding: 15}}>*/}
           {/*<Text style={{fontSize: 18, color: Colors.snow, fontWeight: 'bold'}}>*/}
